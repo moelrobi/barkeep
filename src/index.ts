7614ece7b478a443cef7925e978ded64205f6e12
Robin Möller <moelrobi@gmail.com>
2023-01-26 00:35:20 +0100

    Add Command Logic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 // Import libary for usage
-import { Client, GatewayIntentBits } from "discord.js"
-const { Guilds, MessageContent, GuildMessages, GuildMembers } = GatewayIntentBits
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import { SlashCommand } from './types';
+import { join } from 'path';
+import { readdirSync } from 'fs';
+
+// Set constans
+const { Guilds, MessageContent, GuildMessages, GuildMembers } = GatewayIntentBits;
 const client = new Client({intents: [Guilds, MessageContent, GuildMessages, GuildMembers]});
 
 // Load configuration
 const config = require('../config/config.json');
+
+// Create slash Commands Collection
+client.slashCommands = new Collection<string, SlashCommand>();
+client.cooldowns = new Collection<string, number>();
+
+// Load
+const handlersDir = join(__dirname, './handlers');
+readdirSync(handlersDir).forEach(handler => {
+    require(`${handlersDir}/${handler}`)(client, config);
+})
 
 // Login the bot.
 client.login(config.token);
