ac024122f6c3913c9e019009cc88558185232e65
Robin Möller <moelrobi@gmail.com>
2023-02-26 23:40:39 +0100

    simplefy Interaction Event

--- src/events/Interaction.ts
+++ src/events/Interaction.ts
@@ -1,36 +1,14 @@
-import { BotEvent, ContextMenu, SlashCommand } from "src/types";
+import { BotEvent, ContextMenu } from "src/types";
 import { Interaction } from "discord.js";
 
 const event: BotEvent = {
     name: "interactionCreate",
     execute: (interaction: Interaction) => {
-        if (interaction.isChatInputCommand()) {
-            let command : SlashCommand = interaction.client.slashCommands.get(interaction.commandName);
-            let cooldown : number = interaction.client.cooldowns.get(`${interaction.commandName}-${interaction.user.tag}`);
-            if (!command) return;
-            if (command.cooldown && cooldown) {
-                if (Date.now() < cooldown) {
-                    interaction.reply(`Hey! Du musst noch ${Math.floor(Math.abs(Date.now() - cooldown) / 1000)} Sekunden warten bis du den Befehl wieder nutzen kannst.`);
-                    setTimeout(() => interaction.deleteReply(), 5000);
-                }
-                interaction.client.cooldowns.set(`${interaction.commandName}-${interaction.user.tag}`, Date.now() + command.cooldown * 1000);
+        if (!interaction.isContextMenuCommand()) return;
+        let menu: ContextMenu = interaction.client.contextMenus.get(interaction.commandName);
 
-                setTimeout(() => {
-                    interaction.client.cooldowns.delete(`${interaction.commandName}-${interaction.user.tag}`);
-                }, command.cooldown * 1000);
-            } else if (command.cooldown && !cooldown) {
-                interaction.client.cooldowns.set(`${interaction.commandName}-${interaction.user.tag}`, Date.now() + command.cooldown * 1000);
-            }
-
-            command.execute(interaction);
-        }
-
-        if(interaction.isContextMenuCommand()) {
-            let menu : ContextMenu = interaction.client.contextMenus.get(interaction.commandName);
-
-            if(!menu) return;
-            menu.execute(interaction);
-        }
+        if (!menu) return;
+        menu.execute(interaction);
     }
 }
 
