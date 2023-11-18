a33143c317f53764199fd1fc8eee2cecb92d48a1
Robin Möller <moelrobi@gmail.com>
2023-11-18 15:41:33 +0100

    SWAT Bewerbung

--- src/commands/Vorlage.ts
+++ src/commands/Vorlage.ts
@@ -38,9 +38,9 @@ const command = {
         switch(id) {
             case "sahp_application":
                 var embed = new EmbedBuilder()
-                    .setAuthor({
-                        name: "Vorlage - SAHP | Bewerbung",
-                        iconURL: interaction.client.user.avatarURL()!! 
+                    .setFooter({
+                        text: "Athena-System | made with ❤️ by Spades",
+                        iconURL: interaction.client.user?.avatarURL()!!
                     })
                     .setTitle('Vorlage - SAHP | Bewerbung')
                     .setDescription('Bitte füge die einzelnen Puntke in deinen Bewerbungstext ein.')
@@ -71,6 +71,61 @@ const command = {
                         {
                             name: "**Wieso möchte ich in die San Andreas Highway Patrol?**",
                             value: "Schildere hier wieso du in die San Andreas Highway Patrol eintreten willst."
+                        },
+                        {
+                            name: "**Welche Erfahrungen bringst du mit?**",
+                            value: "Nenne hier welche Erfahrungen du mitbringst."
+                        },
+                        {
+                            name: "**Warum sollten wir dich in die Direktion aufnehmen?**",
+                            value: "Erkläre hier wieso wir dich in die Direktion aufnehmen sollten."
+                        },
+                        {
+                            name: "**Welche Sanktionen hast du bis jetzt im PD erhalten?**",
+                            value: "Nenne hier jegliche Sanktionen die du bereits erhalten hast."
+                        },
+                        {
+                            name: "**Schlusswort**",
+                            value: "Solltest du noch etwas zum Abschluss zu sagen haben, kannst du dies hier tun."
+                        }
+                    ])
+                await interaction.editReply({embeds: [embed]})
+                break;
+            case "swat_application":
+                var embed = new EmbedBuilder()
+                    .setFooter({
+                        text: "Athena-System | made with ❤️ by Spades",
+                        iconURL: interaction.client.user?.avatarURL()!!
+                    })
+                    .setTitle('Vorlage - SWAT | Bewerbung')
+                    .setDescription('Bitte füge die einzelnen Puntke in deinen Bewerbungstext ein.')
+                    .setColor("Blue")
+                    .addFields([
+                        {
+                            name: "**Name und Dienstnummer**",
+                            value: "Trage hier deine Dienstnummer und deinen Namen ein",
+                            inline: true
+                        },
+                        {
+                            name: "**Aktueller Rang**",
+                            value: "Trage hier deinen aktuellen Rang ein",
+                            inline: true
+                        },
+                        {
+                            name: "**Aktuelles Department**",
+                            value: "Nenne hier das Department in dem du aktuell eingesetzt bist"
+                        },
+                        {
+                            name: "**Wie bist du auf unsere Direktion aufmerksam geworden?**",
+                            value: "Erkläre hier in ein paar Sätzen wie du auf uns Aufmerksam geworden bist."
+                        },
+                        {
+                            name: "**Wie würdest du deine Aktivität beurteilen?**",
+                            value: "Erkläre hier wie du deine Aktivität beurteilen würdest."
+                        },
+                        {
+                            name: "**Wieso möchte ich in das Special Weapon and Tactics-Team?**",
+                            value: "Schildere hier wieso du in das Special Weapon and Tactics-Team eintreten willst."
                         },
                         {
                             name: "**Welche Erfahrungen bringst du mit?**",
